56061487e0229d6572bd004a707dbf4ed24abb18
Paul Cavallaro <paulcavallaro@gmail.com>
2016-02-28 18:23:34 -0500

    [Executor] Define the executor interface, add some
    Summary:
    
    Defines the executor interface as well as adding two very simple
    ones, InlineExecutor and QueuedImmediateExecutor.

---
+++ README.md
@@ -0,0 +1,7 @@
+# futures
+
+A project inspired by [folly Futures](https://github.com/facebook/folly/tree/master/folly/futures)
+
+## Notes
+
+Currently (as of 2/28/2016) only works on nightly rust due to usage of FnBox.
---
+++ src/executor.rs
@@ -0,0 +1,103 @@
+use std::boxed::{Box, FnBox};
+use std::cell::UnsafeCell;
+use std::collections::vec_deque::VecDeque;
+
+pub trait Executor {
+    fn add<F>(&self, work: Box<F>) -> ()
+        where F : FnBox();
+}
+
+pub struct InlineExecutor;
+
+/// When work is "queued", execute it immediately inline.
+/// Usually when you think you want this, you actually want a
+/// QueuedImmediateExecutor.
+impl InlineExecutor {
+    pub fn new() -> InlineExecutor {
+        return InlineExecutor
+    }
+}
+
+impl Executor for InlineExecutor {
+    fn add<F>(&self, work: Box<F>) -> ()
+        where F: FnBox() {
+        work.call_box(());
+    }
+}
+
+#[test]
+fn test_inline_executor() {
+    use std::sync::atomic::{AtomicUsize, Ordering};
+
+    let inline = InlineExecutor::new();
+    let cntr = AtomicUsize::new(0);
+    inline.add(Box::new(|| {
+        cntr.fetch_add(1, Ordering::Release);
+    }));
+    let val = cntr.load(Ordering::Acquire);
+    assert_eq!(val, 1);
+}
+
+thread_local!(static QUEUE: UnsafeCell<VecDeque<Box<FnBox()>>>
+              = UnsafeCell::new(VecDeque::new()));
+
+/// Runs inline like InlineExecutor, but with a queue so that any tasks added
+/// to this executor by one of its own callbacks will be queued instead of
+/// executed inline (nested). This is usually better behavior than Inline.
+pub struct QueuedImmediateExecutor;
+
+impl QueuedImmediateExecutor {
+    pub fn new() -> QueuedImmediateExecutor {
+        return QueuedImmediateExecutor;
+    }
+}
+
+impl Executor for QueuedImmediateExecutor {
+    fn add<F>(&self, work: Box<F>) -> ()
+        where F: FnBox(), F: 'static {
+        QUEUE.with(|queue| {
+            unsafe {
+                let queue = queue.get();
+                if (*queue).is_empty() {
+                    (*queue).push_back(work);
+                    while !(*queue).is_empty() {
+                        // TODO(ptc) Since we have to own the Box<FnBox> in order
+                        // to call it we have to pop it off the queue, but that
+                        // changes the queue's size so that future calls to add will
+                        // execute immediately, so we use a place holder we remove
+                        // later.
+                        // Figure out a better way to do this so we
+                        // don't need a placeholder
+                        let fnbox = (*queue).pop_front().unwrap();
+                        (*queue).push_front(Box::new(|| { /* placeholder */ }));
+                        fnbox.call_box(());
+                        let _discarded_placeholder = (*queue).pop_front();
+                    }
+                } else {
+                    (*queue).push_back(work);
+                }
+            }
+        });
+    }
+}
+
+#[test]
+fn test_queued_executor() {
+    use std::sync::atomic::{AtomicUsize, Ordering};
+
+    let queued = QueuedImmediateExecutor::new();
+    let cntr = AtomicUsize::new(0);
+    queued.add(Box::new(|| {
+        queued.add(Box::new(|| {
+            // Should happen last
+            let val = cntr.load(Ordering::Acquire);
+            assert_eq!(val, 1);
+            cntr.fetch_add(1, Ordering::AcqRel);
+        }));
+        let val = cntr.load(Ordering::Acquire);
+        assert_eq!(val, 0);
+        cntr.fetch_add(1, Ordering::AcqRel);
+    }));
+    let val = cntr.load(Ordering::Acquire);
+    assert_eq!(val, 2);
+}
